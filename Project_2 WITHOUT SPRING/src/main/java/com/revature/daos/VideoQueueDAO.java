d19446ea7de78348cfda59fee35ce94b91795252
Mark Ratib <mark.ratib@revature.net>
2021-07-30 13:31:01 -0700

    I MADE A MISTAKE

---
+++ Project_2 WITHOUT SPRING/src/main/java/com/revature/daos/VideoQueueDAO.java
@@ -0,0 +1,162 @@
+package com.revature.daos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.models.Favorite;
+import com.revature.models.User;
+import com.revature.models.Video;
+import com.revature.models.VideoQueue;
+import com.revature.utils.HibernateUtil;
+
+public class VideoQueueDAO implements VideoQueueDAOInterface 
+{
+	/*These are here for easy copy/paste
+	Session ses = HibernateUtil.getSession();
+	Transaction tx = HibernateUtil.startTransaction();
+	*/
+	
+	public void addQueue(VideoQueue newQueue)
+	{
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = HibernateUtil.startTransaction();		
+		ses.save(newQueue);
+		//ACTUALLY ****ING COMMIT IT
+		tx.commit();
+		
+		HibernateUtil.closeSession();
+	}
+
+	//TODO: UPDATE THIS METHOD TO USE THE USERNAME, AND NOT ALLOW USERS TO HAVE 2 QUEUES OF THE SAME NAME
+	public void addToQueue(VideoQueue queue, Video video)
+	{
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = HibernateUtil.startTransaction();
+		
+		String sql = "from VideoQueue where queue_id = '" + queue.getQueue_id() + "'";
+		List<VideoQueue> list = ses.createQuery(sql).list();
+		//if the list is not empty (We got something back)
+		if(list.size() > 0)
+		{
+			//access the first item in the list, access the video list,append the video
+			list.get(0).getVideos().add(video);
+			//update the entry in the DB
+			ses.merge(list.get(0));
+			//commit the transaction
+			tx.commit();
+		}
+		HibernateUtil.closeSession();
+	}
+	
+	// TODO: UPDATE THIS METHOD TO USE THE USERNAME
+	public void removeFromQueue(String name, User user, Video video)//add User to parameters
+	{
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = HibernateUtil.startTransaction();
+		VideoQueue queue = getQueueByName(name);
+		
+		queue.getVideos().remove(video);
+		
+		ses.merge(queue);
+		
+		tx.commit();
+		HibernateUtil.closeSession();
+	}
+
+	public List<VideoQueue> getAllQueues() 
+	{
+		Session ses = HibernateUtil.getSession();
+		List<VideoQueue> list = ses.createQuery("from VideoQueue").list();
+		return list;
+	}
+
+	public VideoQueue getQueueById(int id)
+	{
+		Session ses = HibernateUtil.getSession();
+		
+		VideoQueue target = ses.get(VideoQueue.class, id);
+		
+		HibernateUtil.closeSession();
+		
+		return target;
+	}
+
+	public VideoQueue getQueueByName(String name)//add User to parameters
+	{
+		Session ses = HibernateUtil.getSession();
+		
+		List<VideoQueue> list = getAllQueues();
+		VideoQueue returnList = new VideoQueue();
+		
+		for(VideoQueue q : list)
+		{
+			if(q.getQueue_name().compareTo(name) == 0)
+			{
+				returnList = q;
+				break;
+			}
+		}
+		
+		HibernateUtil.closeSession();
+		return returnList;
+		
+	}
+
+	public List<VideoQueue> getQueuesByUser(User user)
+	{
+		Session ses = HibernateUtil.getSession();
+		
+		List<VideoQueue> totalList = getAllQueues();
+		
+		List<VideoQueue> targetList = new ArrayList<VideoQueue>();
+		
+		for(VideoQueue q : totalList)
+		{
+			if(q.getUser().equals(user))
+			{
+				targetList.add(q);
+			}
+		}
+		
+		return targetList;
+	}
+
+	public void updateQueue(VideoQueue queue)
+	{
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = HibernateUtil.startTransaction();
+		
+		ses.update(queue);
+		
+		tx.commit();
+		HibernateUtil.closeSession();
+
+	}
+
+	public void removeQueue(VideoQueue queue) 
+	{
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = HibernateUtil.startTransaction();
+		
+		ses.delete(queue);
+		
+		tx.commit();
+		HibernateUtil.closeSession();
+
+	}
+
+	public void addToQueue(String name, Video video) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removeFromQueue(String name, Video video) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
---
+++ Project_2 WITHOUT SPRING/src/main/java/com/revature/daos/WatchedDAO.java
@@ -0,0 +1,141 @@
+package com.revature.daos;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.models.User;
+import com.revature.models.Video;
+import com.revature.models.Watched;
+import com.revature.utils.HibernateUtil;
+
+public class WatchedDAO implements WatchedDAOInterface 
+{
+	/*These are here for easy copy/paste
+	Session ses = HibernateUtil.getSession();
+	Transaction tx = HibernateUtil.startTransaction();
+	*/
+
+	
+	public void addWatched(Watched newList) 
+	{
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = HibernateUtil.startTransaction();
+
+		ses.save(newList);
+		//ACTUALLY ****ING COMMIT IT
+		tx.commit();
+		
+		HibernateUtil.closeSession();
+	}
+
+	public Watched getWatchedByUserId(int id) 
+	{
+		Session ses = HibernateUtil.getSession();
+		
+		List<Watched> totalList = getAllWatched();
+		
+		Watched targetList = new Watched();
+		
+		for(Watched w : totalList)
+		{
+			if(w.getUser().getId() == id)
+			{
+				targetList = w;
+				break;
+			}
+		}
+		
+		return targetList;
+	}
+	
+	//Check if a list is empty using a user ID
+	public boolean isEmpty(int id) 
+	{
+		boolean isEmpty = false;
+		
+		Watched targetList = getWatchedByUserId(id);
+		//check if the list is empty
+		if(targetList.getVideos().size() == 0)
+		{
+			//the list is empty, set the boolean to true
+			isEmpty = true;
+		}
+		
+		return isEmpty;
+	}
+
+	public void updateWatched(Watched list) 
+	{
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = HibernateUtil.startTransaction();
+		
+		ses.update(list);
+		
+		tx.commit();
+		HibernateUtil.closeSession();
+		
+	}
+
+	public boolean isPresent(int user_id, int video_id) 
+	{
+		boolean isPresent = false;
+		Session ses = HibernateUtil.getSession();
+		//get the single list into a list of lists
+		List<Watched> list = ses.createQuery("from Watched where user_id = '" + user_id + "'").list();
+		//if we got something back... (list size would be 0 if we didn't)
+		if(list.size() > 0)
+		{
+			//loop through every video in the watched list
+			for(Video v : list.get(0).getVideos())
+			{
+				//if an id in the list is equivalent to the supplied ID...
+				if(v.getId() == video_id)
+				{
+					//the video is in the list
+					isPresent = true;
+					break;
+				}
+			}
+		}
+		
+		return isPresent;
+	}
+
+	public void appendVideo(User user, Video video) 
+	{
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = HibernateUtil.startTransaction();
+		String sql = "from Watched where user_id = '" + user.getId() + "'";
+		List<Watched> list = ses.createQuery(sql).list();
+		//if the list is not empty (We got something back)
+		if(list.size() > 0)
+		{
+			//access the first item in the list, access the video list,append the video
+			list.get(0).getVideos().add(video);
+			//update the entry in the DB
+			ses.merge(list.get(0));
+			//commit the transaction
+			tx.commit();
+		}
+		HibernateUtil.closeSession();
+	}
+
+	public List<Watched> getAllWatched() 
+	{
+		Session ses = HibernateUtil.getSession();
+		List<Watched> list = ses.createQuery("from Watched").list();
+		return list;
+	}
+
+	public boolean removeWatched(Watched list) 
+	{
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = HibernateUtil.startTransaction();
+		ses.delete(list);
+		return false;
+	}
+	
+	
+}

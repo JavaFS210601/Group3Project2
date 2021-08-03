c01b8736f064c9d365bbc7705b94c2b50db377fb
Mark Ratib <mark.ratib@revature.net>
2021-08-03 15:43:50 -0700

    doing a merge

--- FrontEnd/src/app/services/apimovies.service.ts
+++ FrontEnd/src/app/services/apimovies.service.ts
@@ -21,8 +21,6 @@ export class ApimoviesService {
   constructor(private http:HttpClient) { }
 
 
-
-
   searchForMovies(search:string):Observable<Moviesearchresults>
   {
     return this.http.get("https://api.themoviedb.org/3/search/movie?api_key=d31f2229dfccbf4519727987a761bc61&language=en-US&query="+search+"&page=1&include_adult=false") as Observable<Moviesearchresults>;
